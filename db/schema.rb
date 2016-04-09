d0a9d08717955a9c13dbd09303239ad60d0892f4
danamorah <damoraherrera@gmail.com>
2016-04-08 21:59:10 -0300

    five commit

--- db/schema.rb
+++ db/schema.rb
@@ -16,4 +16,19 @@ ActiveRecord::Schema.define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+    create_table "worker", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+    create_table "inventories", force: :cascade do |t|
+    t.string   "sku"
+    t.integer  "wheel_size"
+    t.string  "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer  "genre_id"
+  end
+
 end
